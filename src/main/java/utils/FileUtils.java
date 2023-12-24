9cec8084196a4fe4c1ad08d1d4bb7d51fc5418e8
Maxim Mishchuk <ia14.mishchuk.maxim@gmail.com>
2023-12-25 00:07:56 +0200

    added FileUtils with utility method getFreePath() increased amount of file's metadata sent by P2PFileTransfer

---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package utils;
+
+import org.apache.commons.compress.utils.FileNameUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtils {
+    public static Path getFreePath(Path path) {
+        Path parentPath = path.getParent();
+
+        if (Files.exists(path)) {
+            String name = FileNameUtils.getBaseName(path);
+            String extension = FileNameUtils.getExtension(path);
+
+            for (int i = 1; Files.exists(path); i++) {
+                path = parentPath.resolve(name + "(" + i + ")" + "." + extension);
+            }
+        }
+
+        return path;
+    }
+}
